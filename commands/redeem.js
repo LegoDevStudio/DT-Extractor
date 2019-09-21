093116b2e39da9297974f572b83402223c54f8f1
LegoDevStudio <43821940+LegoDevStudio@users.noreply.github.com>
2019-09-21 10:34:13 +0100

    Update redeem.js

--- commands/redeem.js
+++ commands/redeem.js
@@ -20,7 +20,7 @@ module.exports = class extends Command {
             }
             m.reply("<a:loading:622917463700668423> Creating your role. This can take a few seconds.").then(msg => {
                 // Create a new role with data
-                guild.createRole({
+                m.guild.roles.create({
                     name: args.splice(1).join(" "),
                     color: args[0],
                     position:37,
@@ -32,11 +32,13 @@ module.exports = class extends Command {
                     msg.edit("<@"+m.author.id+"> <a:loading:622917463700668423> Updating Database. Please wait...");
                     queryDb("INSERT INTO `rewards`(`id`, `roleId`) VALUES (?,?)", [m.author.id,role.id]).then((results,tab) => {
                         msg.edit("<@"+m.author.id+"> <:success:622917463234838538> Successfully updated database. You now have your role.");
-                        m.member.addRole(role);
+                        m.member.roles.add(role);
                     }).catch(e => { 
                         msg.edit("<@"+m.author.id+"> <:error:624886916575264778> Whoops! An SQL error was encountered. You have recieved your role, but we are unable to check to see if you are still boosting. LegoDev has been automatically contacted and will manually enter the database entry soon."); 
                         m.guild.members.get("1867301808726343682").user.send(m.author.tag + " redeemed boost reward but unable to automatically alter database. Please execute the following command on the database\n`INSERT INTO \`rewards\`(\`id\`, \`roleId\`) VALUES ('"+m.author.id+"','"+role.id+"')`");
-                        m.member.addRole(role);
+                        m.member.roles.add(role);
+                        console.error(e.stack);
+                    });
                 })
                 .catch(e => { msg.edit("<@"+m.author.id+"> <:error:624886916575264778> Whoops! An error was encountered while attempting to create the role."); console.error(e.stack); })
             });
