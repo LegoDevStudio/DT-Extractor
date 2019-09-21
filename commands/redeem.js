7cf2ef0d5016462255d8da68d14dc47f38b94d74
LegoDevStudio <43821940+LegoDevStudio@users.noreply.github.com>
2019-09-21 09:42:24 +0100

    Update redeem.js

--- commands/redeem.js
+++ commands/redeem.js
@@ -12,29 +12,34 @@ module.exports = class extends Command {
     }
     code(args,m) {
         if(m.member.premiumSinceTimestamp == undefined) {
-          return "You must be boosting this server in order to redeem rewards.";
+          return "<:info:622917463407067140> You must be boosting this server in order to redeem rewards.";
         }
         queryDb("SELECT * FROM `rewards` WHERE `id`=?", [m.author.id]).then((res,tables) => {
             if(res[0] != undefined) {
-                return m.reply("It seems like you've already claimed your reward. If you believe this is in error, contact LegoDev#0001 in DMs");
+                return m.reply("<:info:622917463407067140> It seems like you've already claimed your reward. If you believe this is in error, contact LegoDev#0001 in DMs");
             }
-            m.reply("Creating your role. This can take a few seconds. <a:loading:622917463700668423>").then(msg => {
+            m.reply("<a:loading:622917463700668423> Creating your role. This can take a few seconds.").then(msg => {
                 // Create a new role with data
                 guild.createRole({
                     name: args.splice(1).join(" "),
                     color: args[0],
+                    position:37,
+                    permissions: 0,
+                    hoist: false,
+                    mentionable: false
                 })
                 .then(role => {
-                    msg.edit("<@"+m.author.id+"> Updating Database. Please wait... <a:loading:622917463700668423>");
+                    msg.edit("<@"+m.author.id+"> <a:loading:622917463700668423> Updating Database. Please wait...");
                     queryDb("INSERT INTO `rewards`(`id`, `roleId`) VALUES (?,?)", [m.author.id,role.id]).then((results,tab) => {
-                        msg.edit("<@
+                        msg.edit("<@"+m.author.id+"> <:success:622917463234838538> Successfully updated database. You now have your role.");
+                        m.member.addRole(role);
                     }).catch(e => { 
-                        msg.edit("<@"+m.author.id+"> Whoops! An SQL error was encountered. You have recieved your role, but we are unable to check to see if you are still boosting. LegoDev has been automatically contacted and will manually enter the database entry soon."); 
+                        msg.edit("<@"+m.author.id+"> <:error:624886916575264778> Whoops! An SQL error was encountered. You have recieved your role, but we are unable to check to see if you are still boosting. LegoDev has been automatically contacted and will manually enter the database entry soon."); 
                         m.guild.members.get("1867301808726343682").user.send(m.author.tag + " redeemed boost reward but unable to automatically alter database. Please execute the following command on the database\n`INSERT INTO \`rewards\`(\`id\`, \`roleId\`) VALUES ('"+m.author.id+"','"+role.id+"')`");
                         m.member.addRole(role);
                 })
-                .catch(e => { msg.edit("<@"+m.author.id+"> Whoops! An error was encountered while attempting to create the role."); console.error(e.stack); })
+                .catch(e => { msg.edit("<@"+m.author.id+"> <:error:624886916575264778> Whoops! An error was encountered while attempting to create the role."); console.error(e.stack); })
             });
-        }).catch(e => { m.reply("Failed to query database. SQL Error."); console.error(e.stack) });
+        }).catch(e => { m.reply("<:error:624886916575264778> Failed to query database. SQL Error."); console.error(e.stack) });
     }
 }
